8c0d4bfba28119a916d069db17af0a055dae87f8
MrF1uffy <112059516+MrF1uffy@users.noreply.github.com>
2022-09-04 20:28:22 +0800

    Add files via upload

---
+++ Create and Share Your Jupyter Notebook.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # "My Jupyter Notebook on IBM Watson Studio"
+
+# **Lim Jai Chen**
+
+# *Im interested in data science because I want to switch to an industry that can have a higher earning potential*
+
+# ### Your code, as described in Cell 4. It must be executed and must display an output. Try to keep the code simple (it can be "1+1").
+
+# In[2]:
+
+
+2+2
+
+
+# ***horizontal rule
+# - bulleted list
+# 
+# 1 Numbered list
+# 2 Numbered list
+# 
+
+# In[ ]:
+
+
+
+
